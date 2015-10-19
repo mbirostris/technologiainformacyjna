0abe2348d0c3069d3e81d9c5729d8c637bdbb9fd
john <mynewemail@whatever.com>
2015-10-19 07:45:02 -0700

    jed

--- zajecia02/zajecia02/zajecia03.py
+++ zajecia02/zajecia02/zajecia03.py
@@ -1,4 +1,5 @@
 ﻿import os
+
 #import math
 #from math import pi
 import math as m
@@ -6,12 +7,13 @@ import math as m
 #Proszę napisać program do obliczania pola i obwodu koła. 
 
 def wheelvolume(radius):
-    return m.pi*(int(radius))**2
+    return m.pi*int(radius)**2
 
 def wheelcirkumference(radius):
-    return 2*math.pi*radius;
+    return 2*m.pi*radius;
 
-print(wheelvolume(2))
+#print('Pole kola: ', wheelvolume(2))
+#print('Obwod kola: ', wheelcirkumference(4))
 
 #Zadania z http://brain.fuw.edu.pl/edu/index.php/TI/P%C4%99tle
 
