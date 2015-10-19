904354f2ef0dbca5c7282e464bbd1722a0ea2b81
john <mynewemail@whatever.com>
2015-10-19 07:16:42 -0700

    sda

--- zajecia02/zajecia02/zajecia03.py
+++ zajecia02/zajecia02/zajecia03.py
@@ -1,3 +1,20 @@
 ﻿import os
+#import math
+#from math import pi
+import math as m
 
-print("ala ma kota")
+#Proszę napisać program do obliczania pola i obwodu koła. 
+
+def wheelvolume(radius):
+    return m.pi*(int(radius))**2
+
+def wheelcirkumference(radius):
+    return 2*math.pi*radius;
+
+print(wheelvolume(2))
+
+#Zadania z http://brain.fuw.edu.pl/edu/index.php/TI/P%C4%99tle
+
+
+
+#Zadania z http://www.ling.gu.se/~lager/python_exercises.html
