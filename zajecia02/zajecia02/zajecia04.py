d93758dbf00a0dc97bdc5d0a8eb1703e6bc99415
john <mynewemail@whatever.com>
2015-10-26 08:10:32 -0700

    zaj4

---
+++ zajecia02/zajecia02/zajecia04.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#Enter konczy wywolanie funkcji
+def zadanie1v3():
+    lista = []
+    suma = 0;
+    while True:
+        liczba = input('Podaj liczb?, enter konczy: ')
+        if not liczba:
+            break;
+        a = float(liczba);
+        lista.append(a);
+        suma += a;
+    dlugosc = len(lista)
+    srednia = suma/dlugosc;
+    print('Liczba wprowadzonych liczb: ', dlugosc, '; Srednia liczb: ' ,srednia)
+
+zadanie1v3()
+ 
+
+#Prosz? napisa? funkcj?, kt?ra:
+#1. Kt?ra wypisuje sum? dw?ch argument?w.
+#2. Kt?ra zwraca sum? dw?ch argument?w.
+#3. Wypisuj?c? wynik dzielenia zmiennoprzecinkowego dla dw?ch dowolnych argument?w b?d?cych liczbami.
+#4. Obliczaj?c? i zwracaj?c? pierwiastek zadanego stopnia ? w li?cie parametr?w podajemy liczb? podpierwiastkow? i stopie? pierwiastka, domy?lnie niech wynosi on dwa.
+#5. Wypisuj?c? r?wnanie prostej przechodz?cej przez wsp??rz?dne dw?ch zadanych punkt?w.
+#6. Wypisuj?c? jedno lub dwa rozwi?zania r?wnania kwadratowego a x^2+b x +c = 0, lub informuj?c?, ?e nie ma rozwi?za? rzeczywistych. Parametrami funkcji niech b?d? a, b, c ? wsp??czynniki r?wnania kwadratowego.
+#7. Zwracaj?c? silni? argumentu. Zadanie prosz? rozwi?za? na dwa sposoby ? raz z u?yciem p?tli, a drugi raz rekurencyjnie.
+#8. Wypisuj?c? ??dan? liczb? wyraz?w ci?gu Fibbonacciego. (F_0=0, F_1=1, F_n=F_{n-1}+F_{n-2})
+#9. S?u??c? do obliczania n-tego elementu ci?gu arytmetycznego o zadanym wyrazie pocz?tkowym i r??nicy. Funkcja powinna dawa? mo?liwo?? wypisywania wynik?w po?rednich (element?w ci?gu od pierwszego do przedostatniego obliczonego).
+#10. S?u??c? do obliczania n-tego elementu ci?gu geometrycznego o zadanym wyrazie pocz?tkowym i ilorazie. Funkcja powinna dawa? mo?liwo?? wypisywania wynik?w po?rednich (element?w ci?gu od pierwszego do przedostatniego obliczonego).
+#11. S?u??c? do obliczania n-tej sumy cz??ciowej ci?gu arytmetycznego o zadanym wyrazie pocz?tkowym i r??nicy. Funkcja powinna dawa? mo?liwo?? wypisywania wynik?w po?rednich (kolejno obliczanych element?w ci?gu i sum).
+#12. S?u??c? do obliczania n-tej sumy cz??ciowej ci?gu geometrycznego o zadanym wyrazie pocz?tkowym i ilorazie. Funkcja powinna dawa? mo?liwo?? wypisywania wynik?w po?rednich (kolejno obliczanych element?w ci?gu i sum).
+#13. S?u??c? do obliczania n-tej sumy cz??ciowej ci?gu 1/n^m, gdzie n i m s? parametrami. Funkcja powinna dawa? mo?liwo?? wypisywania wynik?w po?rednich (kolejno obliczanych element?w ci?gu i sum).
+#14. Funkcj? obliczaj?c? d?ugo?? wektora jedno-, dwu- lub tr?jwymiarowego.
+# opt. 15. Funkcj?, kt?ra zwr?ci napis b?d?cy reprezentacj? podanej liczby ca?kowitej. T? ?konwersj? na napis? wykonaj cyfra po cyfrze.
+
+
+#sito Eratostenesa
