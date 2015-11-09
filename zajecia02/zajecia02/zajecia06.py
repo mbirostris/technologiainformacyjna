419d6c4c91aa256dd15c3af692693fe97da0e8e4
john <mynewemail@whatever.com>
2015-11-09 06:52:02 -0800

    ds

---
+++ zajecia02/zajecia02/zajecia06.py
@@ -0,0 +1,56 @@
+
+# -*- coding: utf-8 -*-
+
+# Zdefiniuj funkcj? która wy?wietla s?ownik, w którym kluczami s? liczby od 1 do 3 w??cznie a warto?ciami s? pierwiastki tych liczb
+
+#Zdeniuj funkcj? która wy?wietla s?ownik, gdzie kluczam s? pliczby od 1 do 20, a warto?ci sinusy tych liczb
+
+
+
+#Zrób co wy?ej, ale niech funkcja wy?wietla tylko warto?ci (bez kluczy). Wsk: u?yj keys() lub item()
+
+#Zrób co wy?ej, ale niech funkcja wy?wietla tylko klucze. Wsk: u?yj keys() lub item()
+
+
+
+#Zdefiniuj funkcj? generuj?c? list? warto?ci b?d?cych kwadratami liczb od 1 do 20 (w??cznie)
+
+
+#Korzystaj?c z poprzedniego rozwi?zania napisz funkcj?, która zwraca pierwszych 5 elementów listy z powy?szego zadania.
+
+
+#Powtórz powy?sze zadanie dla ostatnich 5 warto?ci:
+
+#Zdefiniuj funkcj? generuj?c? tuple której warto?ciami s? liczy od 1 do 20:
+
+#Napiz program generuj?cu i wy?witlaj?cy tupl?, której warto?ciami s? liczby parzyste od 1 do 20
+
+
+
+#Napisz funkcj?, która filtruje liczby parzyste z listy. Skorzystaj z funkcji wbudowanej filter() i funkcji lambda.
+#https://docs.python.org/2/library/functions.html#filter
+#http://www.secnetix.de/olli/Python/lambda_functions.hawk
+#https://docs.python.org/3.0/whatsnew/3.0.html
+#http://www.secnetix.de/olli/Python/list_comprehensions.hawk
+
+
+
+#Napisz funkcj? korzystaj?c? z funkji budowanej map() i zrwacjaca list? elementów b?d?cych kwadratami liczb od 1 do 99:
+
+
+
+#http://brain.fuw.edu.pl/edu/index.php/TI/Numpy
+#http://brain.fuw.edu.pl/edu/index.php/TI/Operacje_na_macierzach
+
+#Napisz funkcj? obliczaj?c? ?lad macierzy. 
+
+
+#Napisz funkcj? transponuj?c? macierz kwadratow? w miejscu, czyli bez tworzenia nowej macierzy, poprzez zamian? elementów parami. 
+
+
+#Napisz funkcj? która zwróci transponowan? macierz, niekoniecznie kwadratow?.
+#U?yj funkcji numpy.empty do stworzenia macierzy wynikowej, a nast?pnie wype?nij ja w p?tli. 
+
+
+
+
