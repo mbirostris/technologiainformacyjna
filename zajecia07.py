599391e109387a84c4d5c98059274e6d0fd426c9
guest <guest@debian>
2015-11-19 11:09:42 +0100

    a

--- zajecia07.py
+++ zajecia07.py
@@ -106,6 +106,14 @@ n = int(input('Podaj cyferkę: '))
 for i in EvenGeneratorv3(n):
     print(i, end=',')
 '''
+def print5and7():
+    n=int(input("Podaj cyfrę: "))
+    values = []
+    for i in EvenGeneratorv3(n):
+        values.append(str(i))
+
+    print(",".join(values))
+#print5and7()
 #napisz program, który akceptuje proste wyrażenie matematyczne i wyświetla wynik na stdout. Wsk: https://docs.python.org/2/library/functions.html#eval
 
 #n = input("Podaj wyrazenie: "); print(eval(n))
@@ -132,5 +140,79 @@ pl.show()
 
 ''' Napisz program na znajdowanie najdłuższych linii względnie wyrazów w zawartości pliku.
 '''
+
+'''Znajdowanie najdłuższych linii względnie wyrazów w zawartości pliku.
+Uwaga: wyrazy tutaj liczą się razem z "przyklejonymi" znakami przestankowymi.
+'''
+
+def najdluzsze_linie(plik):
+  '''Zwraca listę linii o maksymalnej długości wśród linii
+  pliku.
+  '''
+  f = open(plik)
+  maxdl = 0
+  najdluzsze = []
+  for linia in f:
+    linia = linia.strip()
+    if len(linia) == maxdl:
+      najdluzsze.append(linia)
+    elif len(linia) > maxdl:
+      maxdl = len(linia)
+      najdluzsze = [linia]
+  f.close()
+  return najdluzsze
+  
+def najdluzsze_wyrazy0(plik):
+  '''Zwraca listę wyrazów o maksymalnej długości wśród wyrazów
+  w treści pliku. Metoda linia po linii.
+  '''
+  f = open(plik)
+  maxdl = 0
+  najdluzsze = []
+  for linia in f:
+    for wyraz in linia.strip().split():
+      if len(wyraz) == maxdl:
+        najdluzsze.append(wyraz)
+      elif len(wyraz) > maxdl:
+        maxdl = len(wyraz)
+        najdluzsze = [wyraz]  
+  f.close()
+  return najdluzsze
+  
+def najdluzsze_wyrazy1(plik):
+  '''Zwraca listę wyrazów o maksymalnej długości wśród wyrazów
+  w treści pliku. Metoda cały plik na raz.
+  '''
+  f = open(plik)
+  tresc = f.read()
+  f.close()
+  wyrazy = tresc.split()
+  maxdl = 0
+  najdluzsze = []
+  for w in wyrazy:
+    if len(w) == maxdl:
+      najdluzsze.append(w)
+    elif len(w) > maxdl:
+      maxdl = len(w)
+      najdluzsze = [w]
+  return najdluzsze
+  
+def najdluzsze_wyrazy(plik):
+  '''Zwraca listę wyrazów o maksymalnej długości wśród wyrazów
+  w treści pliku. Metoda zwięzła, ale ciut wolniejsza.
+  '''
+  f = open(plik)
+  wyrazy = f.read().split()
+  f.close()
+  maxdl = max(len(w) for w in wyrazy)
+  najdluzsze = [w for w in wyrazy if len(w)==maxdl]
+  return najdluzsze
+
+if __name__ == '__main__':
+  from sys import argv
+  plik = argv[1]
+  for linia in najdluzsze_wyrazy(plik):
+    print(linia)
+
 
 #http://brain.fuw.edu.pl/edu/index.php/TI/Operacje_na_macierzach: rozwiąż zadania z mnożeniem i transpozycją macierzy
