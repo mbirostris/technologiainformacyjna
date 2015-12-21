725acf81ba41b3aca30d00fa0c800ffd20bfc34c
guest <guest@debian>
2015-12-21 17:03:59 +0100

    plik

--- zajecia12.py
+++ zajecia12.py
@@ -61,14 +61,36 @@ a = Kolo(30);
 fig = a.draw();
 plt.show()
 '''
-
-
 '''
-import matplotlib.patches as patches
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 
 
+# Calculate spiral coordinates for the Xmas tree
+theta = np.linspace(-8 * np.pi, 8 * np.pi, 200) 
+z = np.linspace(-3, 0, 200)
+r = 5
+x = r * np.sin(theta)*z
+y = r * np.cos(theta)*z
+print(z)
+
+# Use matplotib and its OOP interface to draw it 
+fig = plt.figure() # Create figure
+ax = fig.gca(projection='3d') # It's a 3D Xmas tree!
+#ax.view_init(15, 0) # Set a nice view angle
+#ax._axis3don = False # Hide the 3d axes
+
+# Plot the Xmas tree as a line
+ax.plot(x, y, z,c='green', linewidth=2.5)
+
+# Every Xmas tree needs a star
+#http://matplotlib.org/api/axes_api.html + do scatter
+#ax.scatter(0, 0, 0.2, c='red', s=251, marker='*')
+
+# Type here your best whishes
+#ax.set_title("Merry Christmas")
+plt.show()
+'''
 
 
 '''
@@ -76,5 +98,26 @@ from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages('kometa.pdf')
 #http://matplotlib.org/users/image_tutorial.html
 import matplotlib.image as mpimg
+img=mpimg.imread('comet.png')
+
+plt.imshow(img)
+
+
+#invert colors
+img = img[:,:,0]
+#zmień naświtlenie
+imgfig = plt.imshow(img, cmap="hot")
+#to samo ale po zdefiniowaniu obrazka
+imgfig.set_cmap('spectral')
+#dodanie skali
+plt.colorbar()
+
+#zapis do pliku - jak rysujemy to wykomentowujemy albo przenosimy na koniec 'plt.show()'
+plt.draw()
+pp.savefig()
+pp.close()
+plt.show()
+'''
+
 
 #Wyjątki
