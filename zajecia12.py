b50a6adb45af3ca01ab114ab4352d6bf7c644c2c
guest <guest@debian>
2015-12-21 15:59:45 +0100

    zad12

---
+++ zajecia12.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+#Uzupełnij klasę kwadrat/koło o metodę rysującą kwadrat/koło
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+#http://matplotlib.org/api/patches_api.html
+#http://matplotlib.org/api/figure_api.html#matplotlib.figure.Figure.gca
+
+import matplotlib.patches as patches
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+
+
+
+
+'''
+from matplotlib.backends.backend_pdf import PdfPages
+pp = PdfPages('kometa.pdf')
+#http://matplotlib.org/users/image_tutorial.html
+import matplotlib.image as mpimg
+
+#Wyjątki
