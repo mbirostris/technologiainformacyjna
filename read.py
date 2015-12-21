9cb971d17cdf5b0acffdf0e313e324a73bce28c9
guest <guest@debian>
2015-12-21 17:59:55 +0100

    d

--- read.py
+++ read.py
@@ -7,7 +7,7 @@ from numpy import genfromtxt
 my_data = genfromtxt('plik.csv', delimiter=',')
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax = fig.gca(projection='3d')
+#ax = fig.gca(projection='3d')
 
 ax.plot(my_data[:,0], my_data[:,1], my_data[:,2], c='g')
 ax.set_xlabel("X Axis")
